cf11598aa2485eac86693119825c25849702fd32
Nate Chandler-Smith <nate.chandler-smith@accenture.com>
2020-04-07 15:58:54 -0500

    implements logging to file

--- Worker.cs
+++ Worker.cs
@@ -1,22 +1,30 @@
 using System;
-using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace task
 {
     internal class Worker
     {
         private bool _continue;
+        private ILogger _logger;
 
         public Worker()
         {
             _continue = true;
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder => {
+                builder
+                    .AddFile("app.log", append:true);
+            });
+            _logger = loggerFactory.CreateLogger<Worker>();
         }
 
         private void writeDateTime()
         {
             var dateTime = DateTime.Now;
-            Console.WriteLine($"Checkout the time yo! {dateTime}");
+            string logEntry = $"The time is: {dateTime}";
+            Console.WriteLine(logEntry);
+            _logger.LogInformation(logEntry);
         }
 
         public async Task start()
@@ -24,6 +32,7 @@ namespace task
             while (_continue)
             {
                 writeDateTime();
+                
                 await Task.Delay(1000);
             }
         }
